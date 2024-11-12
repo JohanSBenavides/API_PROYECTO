d5b0773cf1240d49307b31659ab50a67b3555043
Dilan <dilanf1506@gmail.com>
2024-11-11 21:02:18 -0500

    Se hacen cambios en la estructura de la carpeta api, se actualizan lsa versiones de algunas librerias dentro de requirements,txt ya que presentaban dependencias erroneas por conflictos entre versiones, se da inicio a la practica del ORM del proyecto

---
+++ flaskr/app.py
@@ -0,0 +1,19 @@
+from flaskr import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+USER_DB = 'root'
+PASS_DB = ''
+URL_DB = 'localhost'
+NAME_DB = 'phphone'
+FULL_URL_DB = f'mysql+pymysql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = FULL_URL_DB
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+migrate= Migrate()
+migrate.init_app(app, db)

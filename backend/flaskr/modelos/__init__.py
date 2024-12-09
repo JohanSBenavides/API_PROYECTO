bf9c22b3350de0bd0feafafb2f8f70595ac32856
dilanxxoouuu <163854087+dilanxxoouuu@users.noreply.github.com>
2024-12-09 06:51:22 -0500

    Delete backend directory
    Se borra version antigua de la API del proyecto

--- backend/flaskr/modelos/__init__.py
+++
@@ -1,5 +0,0 @@
-from .modelo import db, Rol, Usuario, Carrito, Categoria, Factura, Orden, Pago, Producto, Envio, DetalleFactura, DetalleCarrito
-from .esquemas import  RolSchema, UsuarioSchema, CarritoSchema, CategoriaSchema, FacturaSchema, OrdenSchema, PagoSchema, ProductoSchema, EnvioSchema, DetalleCarritoSchema, DetalleFacturaSchema
-
-__all__ = ["Rol", "Usuario","Carrito", "Categoria", "Factura", "Orden", "Pago", "Producto", "Envio", "DetalleFactura", "DetalleCarrito", 
-           "RolSchema", "UsuarioSchema", "CarritoSchema", "CategoriaSchema", "FacturaSchema", "OrdenSchema", "PagoSchema", "ProductoSchema", "EnvioSchema", "DetalleCarritoSchema", "DetalleFacturaSchema"]
